e9cc635f187482aa0a2073ff3be1fa30c33f6daa
sin-en <s10268353@connect.np.edu.sg>
2025-06-13 18:44:40 +0800

    Update AudioManager.cs
    unable to push changes so edited on here

--- AudioManager.cs
+++ AudioManager.cs
@@ -2,112 +2,26 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField]
-    private AudioClip bgm;
-    [SerializeField]
-    private float bgmVolume = 0.5f;
-    [SerializeField]
-    private AudioClip ambientClip;
-    [SerializeField]
-    private float sfxVolume = 1f;
-    private AudioSource bgmSource;
-    private AudioSource ambientSource;
-    private AudioSource sfxSource;
-    public static AudioManager Instance { get; private set; }
+    public AudioClip bgmClip;
+    private AudioSource audioSource;
 
-    void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            SetupAudioSources();
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
     void Start()
     {
-        PlayMusic();
-    }
-    private void SetupAudioSources()
-    {
-        bgmSource = gameObject.AddComponent<AudioSource>();
-        bgmSource.loop = true;
-        bgmSource.volume = bgmVolume;
-        bgmSource.playOnAwake = false;
-
-        ambientSource = gameObject.AddComponent<AudioSource>();
-        ambientSource.loop = true;
-        ambientSource.volume = bgmVolume * 0.7f;
-        ambientSource.playOnAwake = false;
-
-        sfxSource = gameObject.AddComponent<AudioSource>();
-        sfxSource.loop = false;
-        sfxSource.volume = sfxVolume;
-        sfxSource.playOnAwake = false;
-    }
-    public void PlayMusic()
-    {
-        bgmSource.Stop();
-        ambientSource.Stop();
-
-        if (bgm != null)
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource != null && bgmClip != null)
         {
-            bgmSource.clip = bgm;
-            bgmSource.Play();
-        }
-
-        if (ambientClip != null)
-        {
-            ambientSource.clip = ambientClip;
-            ambientSource.Play();
-        }
-
-        Debug.Log("Playing background music and ambient sound");
-    }
-    public void PlaySFX(AudioClip clip, float volume = 1f)
-    {
-        if (clip != null && sfxSource != null)
-        {
-            sfxSource.PlayOneShot(clip, volume * sfxVolume);
+            audioSource.clip = bgmClip;
+            audioSource.loop = true;
+            audioSource.playOnAwake = true;
+            audioSource.Play();
         }
     }
 
-    public void SetBGMVolume(float volume)
+    public void PlaySFX(AudioClip clip, Vector3 position)
     {
-        bgmVolume = Mathf.Clamp01(volume);
-        if (bgmSource != null)
-            bgmSource.volume = bgmVolume;
-        if (ambientSource != null)
-            ambientSource.volume = bgmVolume * 0.7f;
-    }
-
-    public void SetSFXVolume(float volume)
-    {
-        sfxVolume = Mathf.Clamp01(volume);
-        if (sfxSource != null)
-            sfxSource.volume = sfxVolume;
-    }
-
-    public void PauseAll()
-    {
-        bgmSource?.Pause();
-        ambientSource?.Pause();
-    }
-
-    public void ResumeAll()
-    {
-        bgmSource?.UnPause();
-        ambientSource?.UnPause();
-    }
-
-    public void StopAll()
-    {
-        bgmSource?.Stop();
-        ambientSource?.Stop();
-        sfxSource?.Stop();
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, position);
+        }
     }
 }
